9279e361d352b8b061cd5b96ca7472a74aeee6d1
AMahmoud1 <ahmedmahmoud9574@gmail.com>
2025-04-01 17:16:32 +0400

    Add running loop for detector

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ if __name__ == "__main__":
     # Define Argument
     vehicle_detection_model_name='yolov8n.pt'
     license_plate_detector_model_name='license_plate_detector.pt'
+    input_video_path = "input_video.mp4"
 
     # Create License Plates Detector Instance
     license_plates_detector = LicensePlatesDetector(
         vehicle_detection_model_name=vehicle_detection_model_name,
         license_plate_detector_model_name=license_plate_detector_model_name,
-        
+        input_video_path=input_video_path,        
     )
+
+    # Run License Plates Detector
+    license_plates_detector.run()
